95601f3059a1b06a0ceaa18d8b4b392343afe25b
Pedro Julio <soaresjuliopedro@gmail.com>
2021-02-22 16:15:29 -0300

    Separação dos templates e definição inicial do layout com Bootstrap

---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+<footer class="section">
+    <div class="container-fluid text-center">Copyright &copy; 2021 Vitaminado</div>
+</footer>
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vitaminado</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"
+        integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA=="
+        crossorigin="anonymous" />
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body class="bg-color">
+    <nav class="navbar navbar-light bg-nav">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="../img/banana-smoothie.svg" class="logo" alt="Logo Vitaminado">
+            </a>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Receitas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn" href="#">Nova Receita</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
